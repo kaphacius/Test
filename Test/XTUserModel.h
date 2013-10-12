4a5b17253e758547d9bdae92f9b903b316fa183a
Yurii Zadoianchuk <root1@Yuriis-Mac.local>
2013-10-12 01:21:18 -0700

    adds user model files

---
+++ Test/XTUserModel.h
@@ -0,0 +1,13 @@
+//
+//  XTUserModel.h
+//  Test
+//
+//  Created by Yurii Zadoianchuk on 10/12/13.
+//  Copyright (c) 2013 Yurii Zadoianchuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XTUserModel : NSObject
+
+@end
---
+++ Test/XTUserModel.m
@@ -0,0 +1,13 @@
+//
+//  XTUserModel.m
+//  Test
+//
+//  Created by Yurii Zadoianchuk on 10/12/13.
+//  Copyright (c) 2013 Yurii Zadoianchuk. All rights reserved.
+//
+
+#import "XTUserModel.h"
+
+@implementation XTUserModel
+
+@end
